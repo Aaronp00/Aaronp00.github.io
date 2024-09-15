8a1a87fcb79f59b5016a797ef13470bb40b68a77
Aaron Patterson <avpatterson1@outlook.com>
2024-09-15 23:18:08 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/* document.addEventListener("DOMContentLoaded", () , => {})
+
+const images = document.querySelectorAll("img")
+
+for (image of images ) {
+    // feth("th api")
+//.then(Response => respone.json() )
+.then(data => {
+    image.src = data.message
+    //image.width = 100;
+    //image.height = 100;
+    } )
+   
+    
+        
+    
+} */
