e366ded0b2bda2c18083239e1d85ac2e7bdc10b4
Aaron Patterson <avpatterson1@outlook.com>
2024-09-19 01:06:48 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-/* document.addEventListener("DOMContentLoaded", () , => {})
 
-const images = document.querySelectorAll("img")
 
-for (image of images ) {
-    // feth("th api")
-//.then(Response => respone.json() )
-.then(data => {
-    image.src = data.message
-    //image.width = 100;
-    //image.height = 100;
-    } )
-   
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The document is ready.');
     
-        
-    
-} */
+
+    const pop_up = document.getElementById("customPop");
+    const closeNlButton = document.querySelector(".close");
+
+    // Get the form element
+    const form = document.getElementById('newsletterForm');
+
+    // Add event listener for form submission
+    form.addEventListener('submit', function(event) {
+        // Check if the form is valid
+        if (form.checkValidity()) {
+            // Prevent the form from submitting the normal way
+            event.preventDefault();
+
+            // Show the custom modal
+            pop_up.style.display = "block";
+
+            // Optional, clear the form inputs
+            form.reset();
+        } else {
+            // If form is invalid, the browser will show the validation errors
+            event.preventDefault(); // Prevents form submission to allow error display
+            form.reportValidity(); // This triggers the validation messages to show
+        }
+    });
+
+    // Close the popup when the close button is clicked
+    closeNlButton.onclick = function() {
+      pop_up.style.display = "none";
+    }
+
+
+    // Close the popup if the user clicks outside the modal content
+    window.onclick = function(event) {
+        if (event.target == pop_up) {
+          pop_up.style.display = "none";
+        }
+    }
+});
